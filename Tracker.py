6124a9dfac17f893a38673e87587d4a4f3082ce4
yohans0 <yseyoum@students.kennesaw.edu>
2025-04-14 19:29:15 -0400

    Create Tracker.py
    Trying to start code for the stat tracker

---
+++ Tracker.py
@@ -0,0 +1,12 @@
+
+
+def get_NBA_stats():
+  year = input ("Pick an NBA season after 1979: ")
+player = input ("Pick an NBA player: ")
+
+BASE_URL = 'https://www.espn.com/nba/'
+BASE_URL2 = 'https://www.basketball-reference.com'
+PLAYER_LIST_URL = 'http://www.espn.com/nba/players'
+PLAYER_LIST_URL2 = 'https://www.basketball-reference.com/players/'
+PLAYER_ID = ''
+
